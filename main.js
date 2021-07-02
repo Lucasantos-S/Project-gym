63dac9c1c4bd7576f0e9b1563169b0258f5e8b3f
lucasantos-S <lucassantos.dsilv@gmail.com>
2021-07-01 23:03:35 -0300

    menu mobile criado

--- main.js
+++ main.js
@@ -26,6 +26,31 @@ const swiper = new Swiper('.swiper-container', {
 
   });
 
+
+  const btnMobile = document.getElementById('btn-mobile')
+
+  function toggleMenu(event){
+      if(event.type === 'touchstart') event.preventDefault()
+      const nav = document.getElementById('nav');
+      nav.classList.toggle('active');
+      const active = nav.classList.contains('active');
+
+      event.currentTarget.setAttribute('arial-expanded', 'active');
+      if(active){
+
+        event.currentTarget.setAttribute('arial label', 'Fechar menu');
+
+      } else {
+        if(active)  event.currentTarget.setAttribute('arial label', 'abrir menu');
+
+      }
+      
+  }
+
+  btnMobile.addEventListener('click', toggleMenu);
+  btnMobile.addEventListener('touchstart', toggleMenu);
+
+
   
 
   
