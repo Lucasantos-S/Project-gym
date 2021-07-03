a925061e2788e7e39bc53ef908eebd02939ce315
lucasantos-S <lucassantos.dsilv@gmail.com>
2021-07-02 21:02:54 -0300

    scroll header criado

--- main.js
+++ main.js
@@ -51,6 +51,20 @@ const swiper = new Swiper('.swiper-container', {
   btnMobile.addEventListener('touchstart', toggleMenu);
 
 
+  /* Muda o header da pagina quando der o scroll */
+  const header = document.querySelector("#header")
+  const navHeight = header.offsetHeight 
+  window.addEventListener('scroll', function (){
+    if(window.scrollY >= navHeight) {
+
+      header.classList.add('scroll');
+
+    } else{
+      header.classList.remove('scroll')
+
+    }
+
+  })
   
 
   
